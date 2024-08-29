b44653253bdfee4018a36d5b93cb3b55156e2cca
Nathan Hero Reodica <nathan_hero_reodica@dlsl.edu.ph>
2024-08-29 10:00:36 +0800

    First commit

---
+++ Library management system.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include<fstream>
+#include<string>
+#include <iomanip>
+using namespace std;
+
+class mainMenu{
+	private:
+		int choice;
+		//for adding Books
+		string name;
+		string author;
+		string year;
+		
+		
+		public:
+			void addBook(){
+				
+				ofstream book("book.txt", ios::app);
+				system("cls");
+				cout<<"[Warning: Do not use space]\n";
+				
+				cout << "Add name of book: ";
+				cin >> name;
+				cout << "Add author: ";
+				cin >> author;
+				cout << "Add year of publication: ";
+				cin >> year;
+				
+				if(book.is_open()){
+					book << name << " " << author << " " << year << " " << endl;
+					
+					cout << "\nData added successfully\n";
+				}
+			
+					cout << endl;
+					system("pause");
+					system("cls");
+			
+			}
+			
+			void searchBook(){
+				bool found = false;
+				ifstream book("book.txt");
+				string keyword;
+				
+				cout << "Enter Name of book: ";
+				cin >> keyword;
+				
+				while (book >> name >> author >> year){
+						
+					if (keyword != name){
+					cout << "Book not found!\n";
+					
+					cout << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+				
+					if (keyword == name)
+					cout << "\nBook Found!\n";
+					cout << "\n\nBook Name: " << setw(10) << name;
+					cout << "\nAuthor: "<< setw(10) << author;
+					cout << "\nYear: "<< setw(10) << year;
+					
+					found = true;
+				
+					cout << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+			}
+			
+			void display(){
+				ifstream book("book.txt");
+				cout << "Book\t\t\t\tAuthor\t\t\tYear\n";
+				while (book >> name >> author >> year){
+					cout<< setw(20) << name << setw(20) << author << setw(20) << year << endl; 
+				}
+				cout << endl;
+					system("pause");
+					system("cls");
+			}
+		
+};
+
+
+
+
+int main(){
+	mainMenu m1;
+	int choice;
+	
+	while(true){
+	
+	cout << "===========[MainMenu]===========\n";
+	cout << "[1]Add Book\n[2]Display Books\n[3]Search Book\n[4]Exit\n";
+	cout << "===========[UserInput]===========\n";
+	cin >> choice;
+	
+	switch(choice){
+	
+	case 1:
+	m1.addBook();
+	break;
+	
+	case 2:
+	m1.display();
+	break;
+	
+	case 3:
+	m1.searchBook();
+	break;
+	
+	case 4:
+		cout << "Happy Reading XD";
+		return 0;
+
+	}
+	
+	
+}
+}
